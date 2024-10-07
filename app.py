5a1c124d1680d3aa51b7222b04c9c149c04cc2a0
Kakalasek <honziccekkj@seznam.cz>
2024-10-07 09:06:58 +0200

    basic layout

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        return request.form.to_dict(flat=True)
+    return render_template('home.html')
